1be23b78db9087d82b708898d0e5e4cb83875faf
McCoy1701 <jkellum819@gmail.com>
2025-05-27 19:14:44 -0400

    things added and changed

--- src/dStrings.c
+++ src/dStrings.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-extern char* d_createStringFromFile(const char *filename)
+char* d_createStringFromFile(const char *filename)
 {
   long fileSize;
   char *fileString;
--- tests/test_dDynamicArray.c
+++ tests/test_dDynamicArray.c
@@ -1,9 +1,40 @@
+#include <assert.h>
+#include <stdio.h>
+#include <stdlib.h>
 
+#include "Daedalus.h"
 #include "test.h"
 
 int test_d_initArray( void )
 {
+  size_t cap = 10;
+  size_t int_size = sizeof(int);
   
+  DynamicArray_t* arr = d_initArray( cap, int_size );
+
+  assert( arr != NULL );
+  assert( arr->data != NULL );
+
+  assert( arr->capacity == cap );
+  assert( arr->element_size == int_size );
+  assert( arr->capacity == 0 );
+
+  int* data = (int*)arr->data;
+  for ( size_t i = 0; i < arr->capacity; i++ )
+  {
+    data[i] = i;
+  }
+  
+  for ( size_t i = 0; i < arr->capacity; i++ )
+  {
+    assert( data[i] = i );
+  }
+  
+  printf( "d_initArray passed\n" );
+
+  free( arr->data );
+  free( arr );
+
   return 0;
 }
 
